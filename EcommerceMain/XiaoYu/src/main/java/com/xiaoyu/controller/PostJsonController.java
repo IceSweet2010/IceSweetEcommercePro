2ff70aee2fa249b0110fb3d153ad63f450370cf1
IceSweet2010 <1163514235@qq.com>
2020-01-13 02:32:19 -0800

    commitlog0113002

--- EcommerceMain/XiaoYu/src/main/java/com/xiaoyu/controller/PostJsonController.java
+++ EcommerceMain/XiaoYu/src/main/java/com/xiaoyu/controller/PostJsonController.java
@@ -16,9 +16,23 @@ public class PostJsonController {
 	public String postManTest(@RequestBody UserModel user) {
 		System.out.println("username is:" + user.GetUsername());
 		System.out.println("password is:" + user.GetPassword());
-		return "ok" + user.GetUsername() + user.GetPassword();
+		String str = "[{\"xyid\":\"1\",\"xysay\":\"你\"},{\"xyid\":\"2\",\"xysay\":\"好\"},{\"xyid\":\"3\",\"xysay\":\"小\"},{\"xyid\":\"4\",\"xysay\":\"玉\"}]";
+		return str;
 	}
 	
+	@RequestMapping(value = "/itsright", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	public String postRight(@RequestBody UserModel user) {
+		System.out.println("username is:" + user.GetUsername());
+		System.out.println("password is:" + user.GetPassword());
+		
+		return "ok"+user.GetPassword();
+	}
+	/*@RequestMapping(value = "/postjson", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	public String postManTest(@RequestBody UserModel user) {
+		System.out.println("username is:" + user.GetUsername());
+		System.out.println("password is:" + user.GetPassword());
+		return "ok" + user.GetUsername() + user.GetPassword();
+	}*/
 	//String str = "{\"name\": \"小玉\",     \"words\": \"你好\" }";
 	/*public JSONObject inputmywords(@RequestBody String mywords, HttpServletRequest request) {
 		String str = "{\"name\": \"小玉\",     \"words\": \"你好\" }";
